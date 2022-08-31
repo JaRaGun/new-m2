7a79de5f1fb2e9ccc3263f523590c5803280fe80
Jan Rafael Guno <guno.148983@pasay-edsa.sti.edu.ph>
2022-08-31 11:19:31 +0800

    added some things in content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}","./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,js,jsx}","./node_modules/flowbite/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
